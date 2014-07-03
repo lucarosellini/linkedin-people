e715701246549c6bd00c176ff4f1cdf67986c2ee
Alan Moran <bonzofenix@gmail.com>
2014-07-02 18:45:22 -0700

    does not break with overlays

--- linkedin.rb
+++ linkedin.rb
@@ -2,29 +2,33 @@ require 'rubygems'
 require 'debugger'
 require 'watir-webdriver'
 require 'csv'
+puts "ERROR in recevived args: ruby linkedin.rb USERNAME PASSWORD FILE_WITH_NAMES" unless ARGV.size == 3
 
+user, password, filename = *ARGV
+# puts 'Linkedin username/email (bonzofenix@gmail.com):'
+# user = gets.strip
 
-puts 'Linkedin username/email (bonzofenix@gmail.com):'
-user = gets.strip
+# puts 'Linkedin password (xxxxxx):'
+# password = gets.strip
 
-puts 'Linkedin password (xxxxxx):'
-password = gets.strip
-
-browser = Watir::Browser.new
+browser = Watir::Browser.new :chrome
 browser.goto 'https://www.linkedin.com'
+puts browser.title
 browser.text_field(:id => 'session_key-login').set user
 browser.text_field(:id => 'session_password-login').set password
 sleep 1
 browser.button(:value => 'Sign In').click
 
 #search process
-CSV.foreach("vcap-bosh.csv", { :col_sep => ';' }) do |row|
+CSV.foreach(filename, { :col_sep => ';' }) do |row|
   name = row.first
   res = [name]
   browser.text_field(:id => 'main-search-box').set name
-  browser.button(:value => 'Search').click
+  browser.button(:class => 'search-button').click
   unless browser.text.include? '0 results'
+    browser.screenshot.save 'screenshot.png'
     browser.ol(id: 'results').lis(:class  => 'people')[0..2].each_with_index do |l,i|
+      browser.div(id: "voltron-overlay").wait_while_present
       browser.ol(id: 'results').lis(:class  => 'people')[i].a.click
       if browser.div(id: 'background-experience').exists?
         res << browser.div(id: 'background-experience').divs[0].header.text
